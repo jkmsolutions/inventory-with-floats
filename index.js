91ca48e6dcd57132390bcb6911a67396a366ad16
Kevin Rasmusson <kevin@jkmholding.com>
2024-10-11 16:22:16 +0200

    JS-32 added fetching of user inventory

--- index.js
+++ index.js
@@ -1,5 +1,44 @@
 import axios from 'axios';
 
+const API_KEY = process.env.CSINVENTORYAPI_API_KEY;
+
+// typedef inventory
+
+/**
+ * @typedef {Object} Inventory
+ * 
+ * @property {Array} assets
+ * @property {Array} descriptions
+ * @property {Number} total_inventory_count
+ * @property {Number} success
+ * @property {Number} rwgrsn
+ */
+
+/**
+ * Fetches the inventory of a user
+ * 
+ * @param {string} steamid64 of the user
+ * @returns {Promise<Inventory>} the user's inventory
+ */
 const getUserInventory = async (steamid64) => {
-    
+    if (!API_KEY) {
+        throw new Error('No API key provided');
+    }
+    const response = await axios.get(
+        `https://csinventoryapi.com/api/v1/inventory?api_key=${API_KEY}&steamid64=${steamid64}`
+    );
+
+    return response.data;
+}
+
+const parseInventory = async (inventory) => {
+
+}
+
+const addFloatsToParsedInventory = async (parsedInventory) => {
+
+}
+
+const getInventoryWithFloats = async (steamid64) => {
+
 }
